3f6aa81aec1f592d2f0de21870e0f95835aaa413
code.walnoubani <wesam.alnobani@gmail.com>
2019-04-22 00:22:12 +0300

    22042019

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,36 @@
 /*global $, alert, console, window*/
 
+$(function () {
+
+    'use strict';
+
+    //Adjust Header Height.
+
+    var myHeadwe = $('.header');
+
+    myHeadwe.height($(window).height());
+
+    $(window).resize(function () {
+
+        myHeadwe.height($(window).height());
+    });
+
+    // Links Add Active Class.
+
+    $('.links li a').click(function () {
+
+        $(this).parent().addClass('active').siblings().removeClass('active');
+    });
+
+    // Smooth Scroll To Div - Not
+
+    $('.links li a').click(function () {
+
+        $('html, body').animate({
+
+            scrollTop: $('#' + $(this).data('value')).offset().top
+
+        }, 1000);
+    });
+
+});
