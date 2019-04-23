486de0a64479d67acc023af54cd4984247e060f2
code.walnoubani <wesam.alnobani@gmail.com>
2019-04-24 01:25:03 +0300

    24042019

--- js/custom.js
+++ js/custom.js
@@ -33,4 +33,33 @@ $(function () {
         }, 1000);
     });
 
+    // Our Auto Slider Code.
+
+    (function autoSlider () {
+
+        $('.slider .active').each(function () {
+
+            if (!$(this).is(':last-child')) {
+
+                $(this).delay(3000).fadeOut(1000, function () {
+
+                    $(this).removeClass('active').next().addClass('active').fadeIn();
+
+                    autoSlider ();
+                });
+            } else {
+
+                $(this).delay(3000).fadeOut(1000, function () {
+
+                    $(this).removeClass('active');
+
+                    $('.slider div').eq(0).addClass('active').fadeIn();
+
+                    autoSlider ();
+                });
+            }
+        });
+
+    } ());
+
 });
